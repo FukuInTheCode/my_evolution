d4fd382d9f56b5a98d06b31491a0505aff578302
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 16:55:14 +0200

    change in h

--- includes/my.h
+++ includes/my.h
@@ -2,6 +2,12 @@
 #define MYH
 
 #include <stdio.h>
-#include "./test.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "my_evo.h"
 
-void hello(void);
