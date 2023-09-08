e33bef4b979d01ccba9afbe05a98ee1d1b708048
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 17:32:23 +0200

    CHNAGE IN cell_decla

--- includes/my.h
+++ includes/my.h
@@ -12,7 +12,9 @@
 #include "my_evo.h"
 #include "SFML/Graphics.h"
 
-#define CELL_DECLA(nvar) my_cell_t nvar = {.brain = {.size = 0, .name = "brain"}}
+#define CELL_DECLA(nvar) my_cell_t nvar = {.brain = {.size = 0, .name =\
+                                    "brain"}, .attribute = {.name = "atrbt",\
+                                                            .m = 0, .n = 0}}
 
 typedef struct {
     my_matrix_t attribute;
