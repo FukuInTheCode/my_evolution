b9061cb422ab2ad4be400ce1b276229103780e63
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 17:12:14 +0200

    hard code the cell

--- src/main.c
+++ src/main.c
@@ -7,11 +7,14 @@ int main(int argc, char* argv[])
     CELL_DECLA(cell);
     cell.x = 0;
     cell.y = 0;
+    cell.angle = 0;
     uint32_t dim[] = {2, 3, 2};
     cell.brain.dims = dim;
     cell.brain.size = sizeof(dim) / sizeof(dim[0]);
     my_nn_create(&(cell.brain));
     my_nn_print(&(cell.brain));
     my_nn_free(&(cell.brain));
+
+
     return 0;
 }
