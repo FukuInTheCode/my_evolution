b2f8bf60e66e2291c2a9f1f5a3cf88ab74af8058
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 23:01:17 +0200

    chnage in the matrices selctions

--- src/func/my_evo_viz.c
+++ src/func/my_evo_viz.c
@@ -24,15 +24,15 @@ static void handle_show(my_evo_t *evo, sfRenderWindow *window)
     sfRenderWindow_display(window);
 }
 
+
 void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
 {
     sfRenderWindow *window = sfRenderWindow_create(mode, "my_evo",\
                                                 sfDefaultStyle, NULL);
     uint32_t tick = 0;
-    MAT_DECLA(selected);
-    my_matrix_create(evo->pop_size / 2, 2, 1, &selected);
-    MAT_DECLA(unselected);
-    my_matrix_create(evo->pop_size, 1, 1, &unselected);
+    my_matrix_t *selection_matrices;
+    my_matrix_create_array(&selection_matrices, "S", 2,\
+                    evo->pop_size / 2, 2, evo->pop_size, 1);
     uint32_t gen_i = 0;
     uint32_t i_selected = 0;
     while (sfRenderWindow_isOpen(window)) {
