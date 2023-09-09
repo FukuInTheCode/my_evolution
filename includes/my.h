9e0736d8d9d0a3681d666b0029fa49c483474d10
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 13:41:05 +0200

    change in my h

--- includes/my.h
+++ includes/my.h
@@ -28,3 +28,6 @@ typedef struct {
 #define C_COLOR sfRed
 
 #define SIZE 512.
+
+void my_cell_show(sfRenderWindow *window, void *cell, sfVector2f ratio);
+void my_cell_update(void *pop, uint32_t pop_size);
