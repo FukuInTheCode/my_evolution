1e590aef5ca0098d39f6fc3912b9ecc86fb0eaa0
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 22:39:12 +0200

    add a dupliczata func

---
+++ src/func/my_evo_duplica.c
@@ -0,0 +1,23 @@
+#include "../../includes/my.h"
+
+void my_evo_duplica(my_evo_t *evo, my_matrix_t *selected, \
+                                        my_matrix_t *unselected)
+{
+    for (uint32_t i = 0; i < evo->pop_size - i_selected; ++i) {
+        // printf("%zu, %i, %zu\n", evo->pop, (int)unselected->arr[i][0], evo->agent_struct_size);
+        my_cell_t *cell_child = (my_cell_t *)((char *)(evo->pop) + (int)(unselected->arr[i][0]) * evo->agent_struct_size);
+        my_cell_t *cell_parent = (my_cell_t *)((char *)(evo->pop) + (int)(selected->arr[i % i_selected][0]) * evo->agent_struct_size);
+        double *arr = malloc(my_nn_get_n_params(&(cell_parent->brain)) * sizeof(double));
+        my_nn_to_array(&(cell_parent->brain), &arr);
+        for (uint32_t j = 0; j < my_nn_get_n_params(&(cell_parent->brain)); ++j) {
+            if (my_randfloat(0, 1) <= evo->mutation_chance)
+                arr[j] += my_randfloat(-1 * evo->mutation_range, evo->mutation_range);
+        }
+        my_nn_from_array(&(cell_child->brain), arr);
+        free(arr);
+        // reset
+        my_matrix_randint(0, SIZE, 2, &(cell_child->atb), &(cell_parent->atb));
+        cell_parent->color = sfBlue;
+        cell_child->color = sfRed;
+    }
+}
--- src/func/my_evo_viz.c
+++ src/func/my_evo_viz.c
@@ -47,23 +47,6 @@ void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
             // duplica
             MAT_PRINT((*selected));
             MAT_PRINT((*unselected));
-            for (uint32_t i = 0; i < evo->pop_size - i_selected; ++i) {
-                // printf("%zu, %i, %zu\n", evo->pop, (int)unselected->arr[i][0], evo->agent_struct_size);
-                my_cell_t *cell_child = (my_cell_t *)((char *)(evo->pop) + (int)(unselected->arr[i][0]) * evo->agent_struct_size);
-                my_cell_t *cell_parent = (my_cell_t *)((char *)(evo->pop) + (int)(selected->arr[i % i_selected][0]) * evo->agent_struct_size);
-                double *arr = malloc(my_nn_get_n_params(&(cell_parent->brain)) * sizeof(double));
-                my_nn_to_array(&(cell_parent->brain), &arr);
-                for (uint32_t j = 0; j < my_nn_get_n_params(&(cell_parent->brain)); ++j) {
-                    if (my_randfloat(0, 1) <= evo->mutation_chance)
-                        arr[j] += my_randfloat(-1 * evo->mutation_range, evo->mutation_range);
-                }
-                my_nn_from_array(&(cell_child->brain), arr);
-                free(arr);
-                // reset
-                my_matrix_randint(0, SIZE, 2, &(cell_child->atb), &(cell_parent->atb));
-                cell_parent->color = sfBlue;
-                cell_child->color = sfRed;
-            }
             // reset
             i_selected = 0;
             my_matrix_setall(unselected, 0);
