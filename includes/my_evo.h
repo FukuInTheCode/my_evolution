93a9d66f8d40220c222ac398eef3f84d6d513dfc
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 12:30:30 +0200

    start to add a data struct

--- includes/my_evo.h
+++ includes/my_evo.h
@@ -3,4 +3,16 @@
 
 #include "my_neuralnetwork.h"
 
+typedef bool (*selection_func)(void *);
+typedef double (*reward_func)(void *);
+
+typedef struct {
+    uint32_t pop_size;
+    void *pop;
+    uint32_t max_tick_per_gen;
+    selection_func is_selected;
+    double mutation_chance;
+    reward_func get_reward;
+} my_evo_t;
+
 #endif
