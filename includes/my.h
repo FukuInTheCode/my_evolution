79b3e178ca44665504b1c12e87fbccc0357f23fb
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 14:24:08 +0200

    chaneh oin h

--- includes/my.h
+++ includes/my.h
@@ -31,3 +31,4 @@ typedef struct {
 
 void my_cell_show(sfRenderWindow *window, void *cell, sfVector2f ratio);
 void my_cell_update(void *pop, uint32_t pop_size);
+bool my_cell_is_select(void *cell_ptr);
