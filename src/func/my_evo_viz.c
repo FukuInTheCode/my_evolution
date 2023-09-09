c6123980ace45b21115ce8227ab15addd591151c
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 21:29:55 +0200

    bug fix

--- src/func/my_evo_viz.c
+++ src/func/my_evo_viz.c
@@ -10,9 +10,7 @@ void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
         .y = (window_size.y - 2 * RADIUS) / SIZE
     };
     sfEvent event;
-    // window loop
     while (sfRenderWindow_isOpen(window)) {
-        // event loop
         while (sfRenderWindow_pollEvent(window, &event)) {
             if (event.type == sfEvtClosed)
                 sfRenderWindow_close(window);
@@ -20,30 +18,30 @@ void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
 
         // show
         sfRenderWindow_clear(window, sfBlack);
-        for (uint32_t i = 0; i < evo.pop_size; ++i) {
-            void *cell = (void *)((char *)(evo.pop) + i * evo.agent_struct_size);
+        for (uint32_t i = 0; i < evo->pop_size; ++i) {
+            void *cell = (void *)((char *)(evo->pop) + i * evo->agent_struct_size);
             my_cell_show(window, cell, ratio);
         }
         sfRenderWindow_display(window);
 
         // gen algo
 
-        if (tick < evo.max_tick_per_gen) {
-            my_cell_update(evo.pop, evo.pop_size);
+        if (tick < evo->max_tick_per_gen) {
+            my_cell_update(evo->pop, evo->pop_size);
             ++tick;
-        } else if (tick == evo.max_tick_per_gen) {
+        } else if (tick == evo->max_tick_per_gen) {
             // selection
-            for (uint32_t i = 0; i < evo.pop_size; ++i) {
-                void *cell = (void *)((char *)(evo.pop) + i * evo.agent_struct_size);
+            for (uint32_t i = 0; i < evo->pop_size; ++i) {
+                void *cell = (void *)((char *)(evo->pop) + i * evo->agent_struct_size);
                 double cell_reward = my_cell_get_reward(cell);
                 bool is_selected = my_cell_is_select(cell);
-                if (is_selected && i_selected < evo.pop_size / 2) {
+                if (is_selected && i_selected < evo->pop_size / 2) {
                     my_matrix_set(&selected, i_selected, 0, i);
                     my_matrix_set(&selected, i_selected, 1, cell_reward);
                     i_selected++;
                     continue;
                 }
-                if (is_selected && i_selected >= evo.pop_size / 2) {
+                if (is_selected && i_selected >= evo->pop_size / 2) {
                     double min_selected_reward = my_matrix_mincol(&selected, 1);
                     if (cell_reward > min_selected_reward) {
                         uint32_t min_i = my_matrix_find_row_index(&selected, 1, min_selected_reward);
@@ -58,14 +56,14 @@ void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
             ++tick;
         } else {
             // duplica
-            for (uint32_t i = 0; i < evo.pop_size - i_selected; ++i) {
-                my_cell_t *cell_child = (my_cell_t *)((char *)(evo.pop) + (int)(unselected.arr[i][0]) * evo.agent_struct_size);
-                my_cell_t *cell_parent = (my_cell_t *)((char *)(evo.pop) + (int)(selected.arr[i % i_selected][0]) * evo.agent_struct_size);
+            for (uint32_t i = 0; i < evo->pop_size - i_selected; ++i) {
+                my_cell_t *cell_child = (my_cell_t *)((char *)(evo->pop) + (int)(unselected.arr[i][0]) * evo->agent_struct_size);
+                my_cell_t *cell_parent = (my_cell_t *)((char *)(evo->pop) + (int)(selected.arr[i % i_selected][0]) * evo->agent_struct_size);
                 double *arr = malloc(my_nn_get_n_params(&(cell_parent->brain)) * sizeof(double));
                 my_nn_to_array(&(cell_parent->brain), &arr);
                 for (uint32_t j = 0; j < my_nn_get_n_params(&(cell_parent->brain)); ++j) {
-                    if (my_randfloat(0, 1) <= evo.mutation_chance)
-                        arr[j] += my_randfloat(-1 * evo.mutation_range, evo.mutation_range);
+                    if (my_randfloat(0, 1) <= evo->mutation_chance)
+                        arr[j] += my_randfloat(-1 * evo->mutation_range, evo->mutation_range);
                 }
                 my_nn_from_array(&(cell_child->brain), arr);
                 free(arr);
