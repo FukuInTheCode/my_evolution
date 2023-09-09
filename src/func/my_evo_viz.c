b952f1c9eda198fe7bca7f616406c14a228008d3
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 22:35:16 +0200

    suppr the evo_gen

--- src/func/my_evo_viz.c
+++ src/func/my_evo_viz.c
@@ -37,8 +37,40 @@ void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
     while (sfRenderWindow_isOpen(window)) {
         handle_event(window);
         handle_show(evo, window);
-        if (evo->do_per_n_gen == 1)
-            tick = my_evo_do_tick(evo, tick, &selected, &unselected);
+        if (tick < evo->max_tick_per_gen) {
+            my_cell_update(evo->pop, evo->pop_size);
+            ++tick;
+        } else if (tick == evo->max_tick_per_gen) {
+            // selection
+            i_selected = my_evo_do_selection(evo, selected, unselected);
+            printf("%u\n", i_selected);
+        } else {
+            // duplica
+            MAT_PRINT((*selected));
+            MAT_PRINT((*unselected));
+            for (uint32_t i = 0; i < evo->pop_size - i_selected; ++i) {
+                // printf("%zu, %i, %zu\n", evo->pop, (int)unselected->arr[i][0], evo->agent_struct_size);
+                my_cell_t *cell_child = (my_cell_t *)((char *)(evo->pop) + (int)(unselected->arr[i][0]) * evo->agent_struct_size);
+                my_cell_t *cell_parent = (my_cell_t *)((char *)(evo->pop) + (int)(selected->arr[i % i_selected][0]) * evo->agent_struct_size);
+                double *arr = malloc(my_nn_get_n_params(&(cell_parent->brain)) * sizeof(double));
+                my_nn_to_array(&(cell_parent->brain), &arr);
+                for (uint32_t j = 0; j < my_nn_get_n_params(&(cell_parent->brain)); ++j) {
+                    if (my_randfloat(0, 1) <= evo->mutation_chance)
+                        arr[j] += my_randfloat(-1 * evo->mutation_range, evo->mutation_range);
+                }
+                my_nn_from_array(&(cell_child->brain), arr);
+                free(arr);
+                // reset
+                my_matrix_randint(0, SIZE, 2, &(cell_child->atb), &(cell_parent->atb));
+                cell_parent->color = sfBlue;
+                cell_child->color = sfRed;
+            }
+            // reset
+            i_selected = 0;
+            my_matrix_setall(unselected, 0);
+            my_matrix_setall(selected, 0);
+            usleep(1000);
+        }
     }
     sfRenderWindow_destroy(window);
     MAT_FREE(selected);
