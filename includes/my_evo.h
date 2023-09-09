714ed489a3a8d23720b4ce7366c2941207c77415
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 13:15:17 +0200

    change in the my_evo_t

--- includes/my_evo.h
+++ includes/my_evo.h
@@ -5,7 +5,7 @@
 
 typedef bool (*selection_func)(void *);
 typedef double (*reward_func)(void *);
-typedef void (*do_tick)(void *);
+typedef void (*do_update)(void *);
 
 typedef struct {
     uint32_t pop_size;
@@ -14,7 +14,7 @@ typedef struct {
     double mutation_chance;
     size_t agent_struct_size;
     selection_func is_selected;
-    do_tick do_tick;
+    do_updates update;
     reward_func get_reward;
 } my_evo_t;
 
