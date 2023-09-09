3b2a0a254425ea568ca9a8e86856cd6050dd48b7
FukuInTheCode <tomc.code@gmail.com>
2023-09-09 21:53:18 +0200

    chaneg in h

--- src/func/my_evo_gen.c
+++ src/func/my_evo_gen.c
@@ -13,22 +13,22 @@ uint32_t my_evo_do_tick(my_evo_t *evo, uint32_t tick,\
             double cell_reward = my_cell_get_reward(cell);
             bool is_selected = my_cell_is_select(cell);
             if (is_selected && i_selected < evo->pop_size / 2) {
-                my_matrix_set(&selected, i_selected, 0, i);
-                my_matrix_set(&selected, i_selected, 1, cell_reward);
+                my_matrix_set(selected, i_selected, 0, i);
+                my_matrix_set(selected, i_selected, 1, cell_reward);
                 i_selected++;
                 continue;
             }
             if (is_selected && i_selected >= evo->pop_size / 2) {
-                double min_selected_reward = my_matrix_mincol(&selected, 1);
+                double min_selected_reward = my_matrix_mincol(selected, 1);
                 if (cell_reward > min_selected_reward) {
-                    uint32_t min_i = my_matrix_find_row_index(&selected, 1, min_selected_reward);
-                    my_matrix_set(&unselected, i - i_selected, 0, selected.arr[min_i][0]);
-                    my_matrix_set(&selected, min_i, 0, i);
-                    my_matrix_set(&selected, min_i, 1, cell_reward);
+                    uint32_t min_i = my_matrix_find_row_index(selected, 1, min_selected_reward);
+                    my_matrix_set(unselected, i - i_selected, 0, selected.arr[min_i][0]);
+                    my_matrix_set(selected, min_i, 0, i);
+                    my_matrix_set(selected, min_i, 1, cell_reward);
                     continue;
                 }
             }
-            my_matrix_set(&unselected, i - i_selected, 0, i);
+            my_matrix_set(unselected, i - i_selected, 0, i);
         }
         ++tick;
     } else {
@@ -51,8 +51,8 @@ uint32_t my_evo_do_tick(my_evo_t *evo, uint32_t tick,\
         }
         // reset
         i_selected = 0;
-        my_matrix_setall(&unselected, 0);
-        my_matrix_setall(&selected, 0);
+        my_matrix_setall(unselected, 0);
+        my_matrix_setall(selected, 0);
         usleep(1000);
         tick = 0;
         gen_i++;
