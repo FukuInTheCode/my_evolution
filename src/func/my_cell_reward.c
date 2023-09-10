d0d84728f37c4546542b21a6726b69059de159f4
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 18:33:39 +0200

    chaneg in cell _reward

--- src/func/my_cell_reward.c
+++ src/func/my_cell_reward.c
@@ -1,6 +1,6 @@
 #include "../../includes/my.h"
 
-double my_cell_get_reward(void *cell_ptr)
+double my_cell_get_reward(void *cell_ptr, void *pop, uint32_t pop_size)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     return 1. / (SIZE - cell->atb.arr[0][0] + 1.);
