75f2d229b76296fa427575198ad6a74447a2d04b
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 19:41:02 +0200

    chanegs to play

--- src/func/my_cell_select.c
+++ src/func/my_cell_select.c
@@ -11,9 +11,11 @@ bool my_cell_is_select(void *cell_ptr, void *pop, uint32_t pop_size)
 bool my_cell_is_select_2(void *cell_ptr, void *pop, uint32_t pop_size)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
-    if (cell->atb.arr[0][0] < SIZE / 3 * 2)
+    if (cell->atb.arr[0][0] < SIZE / 3. ||\
+                        cell->atb.arr[0][0] > SIZE / 3. * 2.)
         return false;
-    if (cell->atb.arr[1][0] < SIZE / 3 * 2)
+    if (cell->atb.arr[1][0] < SIZE / 3. ||\
+                        cell->atb.arr[1][0] > SIZE / 3. * 2.)
         return false;
     return true;
 }
