fdf2bdeca4f0816ae8fd5bade913462a690e5225
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 18:09:16 +0200

    changes

---
+++ src/func/my_evo_free.c
@@ -0,0 +1,14 @@
+#include "../../includes/my.h"
+
+void my_evo_free(my_evo_t *evo)
+{
+    for (uint32_t i = 0; i < evo.pop_size; ++i) {
+        my_cell_t *cell = (my_cell_t *)((char *)evo.pop +\
+                                        evo.agent_struct_size * i);
+
+        my_nn_free(&(cell->brain));
+        MAT_FREE((cell->atb));
+    }
+
+    free(evo.pop);
+}
