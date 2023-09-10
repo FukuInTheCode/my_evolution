cd4ee31bd507fff94b6c108ebb12b389afce65ed
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 14:14:01 +0200

    bug fix

--- src/func/my_evo_create.c
+++ src/func/my_evo_create.c
@@ -2,11 +2,11 @@
 
 void my_evo_create(my_evo_t *evo)
 {
-    evo.pop = calloc(evo.pop_size, evo.agent_struct_size);
+    evo->pop = calloc(evo->pop_size, evo->agent_struct_size);
     check_alloc(evo->pop);
 
-    for (size_t i = 0; i < evo.pop_size; ++i) {
-        void *cell = (void *)((char *)(evo.pop) + evo.agent_struct_size * i);
+    for (size_t i = 0; i < evo->pop_size; ++i) {
+        void *cell = (void *)((char *)(evo->pop) + evo->agent_struct_size * i);
         my_cell_create(cell);
     }
 }
