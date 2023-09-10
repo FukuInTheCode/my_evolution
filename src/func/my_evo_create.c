1fbb9f5cc0271f90f790150d6480bb5d0288dea6
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 14:07:01 +0200

    craetea a evo_create funf

---
+++ src/func/my_evo_create.c
@@ -0,0 +1,26 @@
+#include "../../includes/my.h"
+
+void my_evo_create(my_evo_t *evo)
+{
+    evo.pop = calloc(evo.pop_size, evo.agent_struct_size);
+    if (evo.pop == NULL) {
+        fprintf(stderr, "Alloc failed!");
+        exit(1);
+    }
+
+    uint32_t dims[] = {2, 3, 2};
+    for (size_t i = 0; i < evo.pop_size; ++i) {
+        my_cell_t *cell = (my_cell_t *)((char *)(evo.pop) + evo.agent_struct_size * i);
+        cell->brain.dims = dims;
+        cell->brain.size = 3;
+        cell->brain.name = "cell";
+        cell->brain.acti_type = base_type;
+        cell->brain.funcs.af = my_nn_sin;
+        cell->brain.funcs.grad_af = my_nn_sin_grad;
+        my_nn_create(&(cell->brain));
+        my_matrix_create(2, 1, 1, &(cell->atb));
+        my_matrix_randint(0, SIZE, 1, &(cell->atb));
+        cell->atb.name = "atb";
+        cell->color = sfRed;
+    }
+}
--- src/main.c
+++ src/main.c
@@ -9,8 +9,6 @@ int main(int argc, char* argv[])
 {
 
     srand(69);
-    // uint32_t max_reward_id;
-    // uint32_t gen_i = 0;
 
     // my_evo_t
 
@@ -22,30 +20,9 @@ int main(int argc, char* argv[])
         .agent_struct_size = sizeof(my_cell_t),
         .do_per_n_gen = 1
     };
-    evo.pop = calloc(evo.pop_size, evo.agent_struct_size);
-    if (evo.pop == NULL) {
-        fprintf(stderr, "Alloc failed!");
-        exit(1);
-    }
     // used var
 
     // population creattion (main)
-
-    uint32_t dims[] = {2, 3, 2};
-    for (size_t i = 0; i < evo.pop_size; ++i) {
-        my_cell_t *cell = (my_cell_t *)((char *)(evo.pop) + evo.agent_struct_size * i);
-        cell->brain.dims = dims;
-        cell->brain.size = 3;
-        cell->brain.name = "cell";
-        cell->brain.acti_type = base_type;
-        cell->brain.funcs.af = my_nn_sin;
-        cell->brain.funcs.grad_af = my_nn_sin_grad;
-        my_nn_create(&(cell->brain));
-        my_matrix_create(2, 1, 1, &(cell->atb));
-        my_matrix_randint(0, SIZE, 1, &(cell->atb));
-        cell->atb.name = "atb";
-        cell->color = sfRed;
-    }
 
     sfVideoMode mode = {1500, 1500, 32};
     my_evo_viz(&evo, mode);
