3f68caa9c78169a4583cebf5fc9cb50085103bce
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 18:11:59 +0200

    chaneg in my_evo_free

--- src/func/my_evo_free.c
+++ src/func/my_evo_free.c
@@ -3,8 +3,9 @@
 void my_evo_free(my_evo_t *evo)
 {
     for (uint32_t i = 0; i < evo.pop_size; ++i) {
-        my_cell_t *cell = (my_cell_t *)((char *)evo.pop +\
+        void *cell = (void *)((char *)evo.pop +\
                                         evo.agent_struct_size * i);
+        my_cell_free(cell);
     }
 
     free(evo.pop);
