3c3287f9411ace2325d33b4bea6ceaeaff7ae077
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 18:32:25 +0200

    change in cell selct

--- src/func/my_cell_select.c
+++ src/func/my_cell_select.c
@@ -1,6 +1,6 @@
 #include "../../includes/my.h"
 
-bool my_cell_is_select(void *cell_ptr)
+bool my_cell_is_select(void *cell_ptr, void *pop, uint32_t pop_size)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     if (cell->atb.arr[0][0] < SIZE / 3 * 2)
