599638918764c37d3ecd8b84b2ab1100cbbed01d
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 19:35:26 +0200

    changes

--- src/func/my_cell_update.c
+++ src/func/my_cell_update.c
@@ -22,7 +22,7 @@ static void check_new_atb(my_matrix_t *old_atb, uint32_t pop_size,\
     if (new_atb->arr[1][0] < 0)
         new_atb->arr[1][0] = 0;
     if (new_atb->arr[0][0] > SIZE)
-        new_atb->arr[0][0] = 0;
+        new_atb->arr[0][0] = SIZE;
     if (new_atb->arr[1][0] > SIZE)
         new_atb->arr[1][0] = SIZE;
     for (uint32_t i = 0; i < pop_size; ++i) {
