3000d7d588b8cdcb730d1934da99b4f4734acf03
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 18:31:44 +0200

    change in the h

--- includes/my_evo.h
+++ includes/my_evo.h
@@ -6,7 +6,7 @@
 
 typedef void (*draw_func)(sfRenderWindow *, void *);
 typedef void (*update_func)(void *, void *, uint32_t);
-typedef bool (*selection_func)(void *);
+typedef bool (*selection_func)(void *, void *, uint32_t);
 typedef double (*reward_func)(void *);
 typedef uint32_t (*get_array_func)(void *, double **);
 typedef void (*reset_func)(void *, bool);
