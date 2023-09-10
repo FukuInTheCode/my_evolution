fd9a5399ce75058b57d4fdeb57c80536d4d72006
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 18:23:10 +0200

    bug fix

--- src/func/my_evo_free.c
+++ src/func/my_evo_free.c
@@ -2,11 +2,11 @@
 
 void my_evo_free(my_evo_t *evo)
 {
-    for (uint32_t i = 0; i < evo.pop_size; ++i) {
-        void *cell = (void *)((char *)evo.pop +\
-                                        evo.agent_struct_size * i);
+    for (uint32_t i = 0; i < evo->pop_size; ++i) {
+        void *cell = (void *)((char *)evo->pop +\
+                                        evo->agent_struct_size * i);
         evo->free_agent(cell);
     }
 
-    free(evo.pop);
+    free(evo->pop);
 }
