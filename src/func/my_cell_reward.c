9d217a0635ee3ede303ce42831bc92380e62e07a
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 19:25:45 +0200

    adda a get reward 2

--- src/func/my_cell_reward.c
+++ src/func/my_cell_reward.c
@@ -5,3 +5,11 @@ double my_cell_get_reward(void *cell_ptr, void *pop, uint32_t pop_size)
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     return 1. / (SIZE - cell->atb.arr[0][0] + 1.);
 }
+
+double my_cell_get_reward_2(void *cell_ptr, void *pop, uint32_t pop_size)
+{
+    my_cell_t *cell = (my_cell_t *)cell_ptr;
+    double dist = pow(SIZE - cell->atb.arr[0][0] + 1., 2) +\
+                        pow(SIZE - cell->atb.arr[1][0] + 1., 2)
+    return 1. / sqrt(dist);
+}
