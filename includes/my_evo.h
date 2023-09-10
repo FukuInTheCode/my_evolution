ce9658630f2eabedc735014e4486aba25d22c695
FukuInTheCode <tomc.code@gmail.com>
2023-09-10 14:21:46 +0200

    chanegs on my evo

--- includes/my_evo.h
+++ includes/my_evo.h
@@ -4,11 +4,6 @@
 #include "my_neuralnetwork.h"
 #include "SFML/Graphics.h"
 
-typedef bool (*selection_func)(void *);
-typedef double (*reward_func)(void *);
-typedef void (*do_update)(void *, uint32_t);
-typedef void (*show_func)(sfRenderWindow *, void *);
-
 typedef struct {
     size_t pop_size;
     void *pop;
@@ -17,10 +12,6 @@ typedef struct {
     double mutation_range;
     size_t agent_struct_size;
     uint32_t do_per_n_gen;
-    selection_func is_selected;
-    do_update update;
-    reward_func get_reward;
-    show_func show;
 } my_evo_t;
 
 void my_evo_viz(my_evo_t *evo, sfVideoMode mode);
