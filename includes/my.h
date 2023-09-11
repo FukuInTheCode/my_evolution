2722e39adc15d73b742f29de0ba8851c999ac950
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:38:49 +0200

    chnage in h

--- includes/my.h
+++ includes/my.h
@@ -34,6 +34,6 @@ bool my_cell_is_select(void *cell_ptr, void *pop, uint32_t pop_size, void *param
 double my_cell_get_reward(void *cell_ptr, void *pop, uint32_t pop_size, void *params);
 uint32_t my_cell_get_array(void *cell_ptr, double **arr, void *params);
 void my_cell_reset(void *cell_ptr, void *pop, uint32_t pop_size, void *params);
-void my_cell_from_array(void *cell_ptr, double *arr);
+void my_cell_from_array(void *cell_ptr, double *arr, void *params);
 void my_cell_create(void *cell_ptr);
 void my_cell_free(void *cell_ptr);
