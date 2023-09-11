44f54f7c563c4deea47233ed1bab048c57b55f38
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 19:34:10 +0200

    bug fix

--- src/func/my_evo_algo.c
+++ src/func/my_evo_algo.c
@@ -27,7 +27,7 @@ uint32_t my_evo_algo(my_evo_t *evo, my_matrix_t **s_matrices,\
                             uint32_t tick, uint32_t i_selected)
 {
     if (tick == 0) {
-        for (uint32_t i = 0; i < evo->skip_gen * evo->max_tick_per_gen; ++i)
+        for (uint32_t i = 0; i < evo->gen_skip * evo->max_tick_per_gen; ++i)
             uint32_t i_sel_tmp = do_gen(evo, s_matrices, i % evo->max_tick_per_gen, i_sel_tmp);
     }
     return do_gen(evo, s_matrices, tick, i_selected);
