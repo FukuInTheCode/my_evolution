a1a6c5ffd8b94f4c0d90925222063c0485a42b94
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:29:17 +0200

    delete rewqard 2

--- src/func/my_cell_reward.c
+++ src/func/my_cell_reward.c
@@ -5,11 +5,3 @@ double my_cell_get_reward(void *cell_ptr, void *pop, uint32_t pop_size)
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     return 1. / (SIZE - cell->atb.arr[0][0] + 1.);
 }
-
-double my_cell_get_reward_2(void *cell_ptr, void *pop, uint32_t pop_size)
-{
-    my_cell_t *cell = (my_cell_t *)cell_ptr;
-    double dist = pow(0 - cell->atb.arr[0][0] + 1., 2) +\
-                        pow(0 - cell->atb.arr[1][0] + 1., 2);
-    return 1. / sqrt(dist);
-}
