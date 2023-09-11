3aed283022edeae3e0e180b851f06d0fb86a3f23
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 19:01:58 +0200

    chaneg in evo select

--- src/func/my_evo_select.c
+++ src/func/my_evo_select.c
@@ -8,13 +8,13 @@ uint32_t my_evo_do_selection(my_evo_t *evo, my_matrix_t *selected,\
         void *cell = (void *)((char *)(evo->pop) + i * evo->agent_struct_size);
         double cell_reward = evo->get_agent_reward(cell, evo->pop, evo->pop_size);
         bool is_selected = evo->agent_is_select(cell, evo->pop, evo->pop_size);
-        if (is_selected && i_selected < evo->pop_size / 2) {
+        if (is_selected && i_selected < evo->selected_size) {
             my_matrix_set(selected, i_selected, 0, i);
             my_matrix_set(selected, i_selected, 1, cell_reward);
             i_selected++;
             continue;
         }
-        if (is_selected && i_selected >= evo->pop_size / 2) {
+        if (is_selected && i_selected >= evo->selected_size) {
             double min_selected_reward = my_matrix_mincol(selected, 1);
             if (cell_reward > min_selected_reward) {
                 uint32_t min_i = my_matrix_find_row_index(selected, 1, min_selected_reward);
