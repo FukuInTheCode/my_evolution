f3b1d3287088bb7c3e0e22a3e6d3acb8c16c0206
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:27:15 +0200

    chaneg in

--- includes/my_evo.h
+++ includes/my_evo.h
@@ -6,7 +6,7 @@
 
 typedef void (*draw_func)(sfRenderWindow *, void *, void *);
 typedef uint32_t (*update_func)(void *, void *, uint32_t, void *);
-typedef bool (*selection_func)(void *, void *, uint32_t);
+typedef bool (*selection_func)(void *, void *, uint32_t, void *);
 typedef double (*reward_func)(void *, void *, uint32_t);
 typedef uint32_t (*get_array_func)(void *, double **);
 typedef void (*reset_func)(void *, bool, void *, uint32_t);
