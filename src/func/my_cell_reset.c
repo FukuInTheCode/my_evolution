5e5dc9109f6a3322b5876b331dba50613d5a14e7
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:35:45 +0200

    chaneg in cell reset

--- src/func/my_cell_reset.c
+++ src/func/my_cell_reset.c
@@ -4,8 +4,4 @@ void my_cell_reset(void *cell_ptr, void *pop, uint32_t pop_size, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     my_matrix_randint(0, SIZE, 1, &(cell->atb));
-    if (is_selected)
-        cell->color = sfBlue;
-    else
-        cell->color = sfRed;
 }
