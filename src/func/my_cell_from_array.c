f76523b1fabf932e2bfd7c8454fedde43cf2e2c7
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:38:40 +0200

    chnage in from array cell

--- src/func/my_cell_from_array.c
+++ src/func/my_cell_from_array.c
@@ -1,6 +1,6 @@
 #include "../../includes/my.h"
 
-void my_cell_from_array(void *cell_ptr, double *arr)
+void my_cell_from_array(void *cell_ptr, double *arr, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     my_nn_from_array(&(cell->brain), arr);
