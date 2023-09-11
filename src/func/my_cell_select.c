aa05d69b28f2c331b49daa1e8c0c2d2ff6e266c1
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:29:04 +0200

    deleete szelct 2

--- src/func/my_cell_select.c
+++ src/func/my_cell_select.c
@@ -7,15 +7,3 @@ bool my_cell_is_select(void *cell_ptr, void *pop, uint32_t pop_size, void *param
         return false;
     return true;
 }
-
-bool my_cell_is_select_2(void *cell_ptr, void *pop, uint32_t pop_size, void *params)
-{
-    my_cell_t *cell = (my_cell_t *)cell_ptr;
-    if (cell->atb.arr[0][0] < SIZE / 3. ||\
-                        cell->atb.arr[0][0] > SIZE / 3. * 2.)
-        return false;
-    if (cell->atb.arr[1][0] < SIZE / 3. ||\
-                        cell->atb.arr[1][0] > SIZE / 3. * 2.)
-        return false;
-    return true;
-}
