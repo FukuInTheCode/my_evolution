8ef440b1691a3e83ba57ed8f951cfb3fd9ac1a45
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 19:11:22 +0200

    move the do_gen to it own fiel

---
+++ src/func/my_evo_algo.c
@@ -0,0 +1,29 @@
+#include "../../includes/my.h"
+
+static uint32_t do_gen(my_evo_t *evo, my_matrix_t **s_matrices,\
+                                uint32_t tick, uint32_t i_selected)
+{
+    if (tick < evo->max_tick_per_gen) {
+        for (uint32_t i = 0; i < evo->pop_size; ++i) {
+            void *cell = (void *)((char *)evo->pop + i *\
+                                    evo->agent_struct_size);
+            evo->pop_size = evo->update_agent(cell, evo->pop, evo->pop_size);
+        }
+    } else if (tick == evo->max_tick_per_gen) {
+        i_selected = my_evo_do_selection(evo, &((*s_matrices)[0]),\
+                                                    &((*s_matrices)[1]));
+    } else {
+        my_evo_duplica(evo, &((*s_matrices)[0]), &((*s_matrices)[1]),\
+                                                            i_selected);
+        i_selected = 0;
+        my_matrix_setall(&((*s_matrices)[1]), 0);
+        my_matrix_setall(&((*s_matrices)[0]), 0);
+    }
+    return i_selected;
+}
+
+uint32_t my_evo_algo(my_evo_t *evo, my_matrix_t **s_matrices,\
+                            uint32_t tick, uint32_t i_selected)
+{
+    do_gen(evo, s_matrices, tick, i_selected);
+}
--- src/func/my_evo_viz.c
+++ src/func/my_evo_viz.c
@@ -19,28 +19,6 @@ static void handle_show(my_evo_t *evo, sfRenderWindow *window)
     sfRenderWindow_display(window);
 }
 
-static uint32_t do_gen(my_evo_t *evo, my_matrix_t **s_matrices,\
-                                uint32_t tick, uint32_t i_selected)
-{
-    if (tick < evo->max_tick_per_gen) {
-        for (uint32_t i = 0; i < evo->pop_size; ++i) {
-            void *cell = (void *)((char *)evo->pop + i *\
-                                    evo->agent_struct_size);
-            evo->pop_size = evo->update_agent(cell, evo->pop, evo->pop_size);
-        }
-    } else if (tick == evo->max_tick_per_gen) {
-        i_selected = my_evo_do_selection(evo, &((*s_matrices)[0]),\
-                                                    &((*s_matrices)[1]));
-    } else {
-        my_evo_duplica(evo, &((*s_matrices)[0]), &((*s_matrices)[1]),\
-                                                            i_selected);
-        i_selected = 0;
-        my_matrix_setall(&((*s_matrices)[1]), 0);
-        my_matrix_setall(&((*s_matrices)[0]), 0);
-    }
-    return i_selected;
-}
-
 
 void my_evo_viz(my_evo_t *evo, sfVideoMode mode)
 {
