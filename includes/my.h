5f2e088c733fea8b67e346044e5274ea2b8ad716
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:32:20 +0200

    change in h

--- includes/my.h
+++ includes/my.h
@@ -31,7 +31,7 @@ typedef struct {
 void my_cell_draw(sfRenderWindow *window, void *cell, void *params);
 uint32_t my_cell_update(void *cell_ptr, void *pop, uint32_t pop_sizev, void *);
 bool my_cell_is_select(void *cell_ptr, void *pop, uint32_t pop_size, void *params);
-double my_cell_get_reward(void *cell_ptr, void *pop, uint32_t pop_size);
+double my_cell_get_reward(void *cell_ptr, void *pop, uint32_t pop_size, void *params);
 uint32_t my_cell_get_array(void *cell_ptr, double **arr);
 void my_cell_reset(void *cell_ptr, bool is_selected,\
                             void *pop, uint32_t pop_size);
