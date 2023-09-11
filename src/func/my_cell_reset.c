c81a42260ab49e44eb10d07b379be36751cbb695
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 21:35:31 +0200

    chna&eg in cell reset

--- src/func/my_cell_reset.c
+++ src/func/my_cell_reset.c
@@ -1,7 +1,6 @@
 #include "../../includes/my.h"
 
-void my_cell_reset(void *cell_ptr, bool is_selected,\
-                            void *pop, uint32_t pop_size)
+void my_cell_reset(void *cell_ptr, void *pop, uint32_t pop_size, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     my_matrix_randint(0, SIZE, 1, &(cell->atb));
